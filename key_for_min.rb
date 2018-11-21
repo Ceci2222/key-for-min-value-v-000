b04510f28697d7180859320ca61e0112a988a2db
Marie Burns <mcburns2222@gmail.com>
2018-11-21 19:30:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
       lowest = value
     end
   end
-  name.hash.each do |key, value|
+  name_hash.each do |key, value|
     if value == lowest
       puts key
     end
