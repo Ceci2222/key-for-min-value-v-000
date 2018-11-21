8347f5c94a7120fd8a32ff0b597c109e724fd5cf
Marie Burns <mcburns2222@gmail.com>
2018-11-21 19:46:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,7 @@ def key_for_min_value(name_hash)
     if low_number == nil 
       low_number = value
       low_name = key
-    elsif value < low_number
-      low_number = value
-    elsif value == low_number
+    elsif value <= low_number
       low_number = value
       low_name = key
     end
